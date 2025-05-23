5a6cfa354011117ad371f876fc4898ec5aad6437
SHEMA Olivier <114829255+obscode20726@users.noreply.github.com>
2025-05-23 17:51:05 +0200

    Enhance WhoWeAre section with framer-motion for animations, update image loading strategy, and clean up class names for improved readability

--- src/sections/WhoWeAre.tsx
+++ src/sections/WhoWeAre.tsx
@@ -1,6 +1,8 @@
+"use client";
 import ytplaceholder from "@/assets/images/youtube-placeholder.svg";
 import Button from "../components/Button";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function WhoWeAre() {
     const features = [
@@ -20,35 +22,51 @@ export default function WhoWeAre() {
                 "We strive for excellence in education, investing in the latest technology and resources to enhance teaching and learning practices. We are dedicated to innovation and constantly seek ways to better serve our students.",
         },
     ];
+
+    const fadeInUp = {
+        hidden: { opacity: 0, y: 100, scale: 0.8 },
+        show: { opacity: 1, y: 0, scale: 1, transition: { duration: 0.6 } },
+    };
+
     return (
         <section className="bg-white mt-36 pt-16">
             <div className="justify-between py-16">
                 <div className="px-5 md:px-12 justify-center">
                     <div className="mx-auto justify-between self-center w-full max-w-7xl">
                         <div className="w-layout-grid lynx-grid-image-right">
-                            <div className="pl-[5%] pr-[5%] md:pl-[15%]">
-                                <h2 className="autoShow text-center lg:text-left text-[29px] md:text-[34px] lg:text-[46px] xlg:text-[46px] text-[#cc7f00] font-lexend leading-[120%] mt-0 mb-9 font-bold max-w-[540px] md:max-w-[740px]">
+                            <motion.div
+                                className="pl-[5%] pr-[5%] md:pl-[15%]"
+                                variants={fadeInUp}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: false, amount: 0.3 }}
+                            >
+                                <h2 className="text-center lg:text-left text-[29px] md:text-[34px] lg:text-[46px] text-[#cc7f00] font-lexend leading-[120%] mt-0 mb-9 font-bold max-w-[540px] md:max-w-[740px]">
                                     Who we are
                                 </h2>
-                                <p className="autoShow text-center lg:text-left text-[16px] sm:text-[18px] text-slate-800 font-varela leading-[165%] max-w-[820px] mb-3">
+                                <p className="text-center lg:text-left text-[16px] sm:text-[18px] text-slate-800 font-varela leading-[165%] max-w-[820px] mb-3">
                                     High Gate International Academy is a modern
                                     school located in Kigali, Rwanda. The school
                                     offers daycare, nursery and primary
                                     education in a nurturing and inclusive
                                     environment.
                                 </p>
-                                <p className="autoShow text-[14px] lg:text-[16px] font-verdana text-center lg:text-left text-[#024059] my-0 font-normal">
+                                <p className="text-[14px] lg:text-[16px] font-verdana text-center lg:text-left text-[#024059] my-0 font-normal">
                                     Your children will have access to:
                                 </p>
+
                                 {features.map((feature, index) => (
-                                    <div
+                                    <motion.div
                                         key={index}
-                                        className="autoShow border-black"
+                                        variants={fadeInUp}
+                                        initial="hidden"
+                                        whileInView="show"
+                                        viewport={{ once: false, amount: 0.3 }}
                                     >
-                                        <h1 className="autoShow text-[#cc7f00] text-center lg:text-left mt-[10px] font-arial leading-[44px] mb-0 text-[20px] font-bold">
+                                        <h1 className="text-[#cc7f00] text-center lg:text-left mt-[10px] font-arial leading-[44px] mb-0 text-[20px] font-bold">
                                             {feature.title}
                                         </h1>
-                                        <p className="autoShow text-center lg:text-left text-[14px] lg:text-[16px] text-[#024059] font-verdana leading-5 my-0 font-normal">
+                                        <p className="text-center lg:text-left text-[14px] lg:text-[16px] text-[#024059] font-verdana leading-5 my-0 font-normal">
                                             {feature.description}
                                         </p>
                                         {index === features.length - 1 && (
@@ -59,43 +77,52 @@ export default function WhoWeAre() {
                                                 Learn more
                                             </Button>
                                         )}
-                                    </div>
+                                    </motion.div>
                                 ))}
-                            </div>
-                            <div className=" autoShow flex flex-row relative justify-center items-stretch">
+                            </motion.div>
+
+                            <motion.div
+                                className="flex flex-row relative justify-center items-stretch"
+                                variants={fadeInUp}
+                                initial="hidden"
+                                whileInView="show"
+                                viewport={{ once: false, amount: 0.3 }}
+                            >
                                 <Image
                                     src="https://cdn.prod.website-files.com/645514bf46c37985244c4dfc/672cad193c4cf22f51c8618a_cat%201%20(1200%20x%201350%20px).jpg"
                                     alt="School image"
-                                    loading="lazy"
-                                    sizes="(max-width: 1215px) 100vw, 1215px"
-                                    className=" animate-float bottom-[-25px] left-[20px] md:bottom-[-25px] md:left-[-1px] lg:bottom-[-25px] lg:left-[-20px] max-w-[40%] absolute z-10 object-cover rounded-[30px] lg:max-w-[56%] shadow-[0_60px_60px_-15px_#1f20221a]"
+                                    loading="eager"
                                     width={1200}
                                     height={1350}
+                                    className="animate-float bottom-[-25px] left-[20px] md:bottom-[-25px] md:left-[-1px] lg:bottom-[-25px] lg:left-[-20px] max-w-[40%] absolute z-10 object-cover rounded-[30px] lg:max-w-[56%] shadow-[0_60px_60px_-15px_#1f20221a]"
                                 />
                                 <Image
                                     src="https://cdn.prod.website-files.com/645514bf46c37985244c4dfc/668d5fa1c0a504eed33ec594_8.jpg"
                                     alt="School image"
-                                    loading="lazy"
-                                    sizes="(max-width: 1215px) 100vw, 1215px"
-                                    className="object-cover animate-float max-w-full rounded-[30px] align-middle"
+                                    loading="eager"
                                     width={1200}
                                     height={800}
+                                    className="object-cover max-w-full rounded-[30px] align-middle"
                                 />
-                            </div>
+                            </motion.div>
                         </div>
-                        <div
-                            className=" relative pt-[56.170213%] mt-[60px] bg-cover w-full pb-0 px-0 "
+                        <motion.div
+                            className=" relative pt-[56.170213%] mt-[60px] bg-cover w-full pb-0 px-0"
                             style={{
                                 backgroundImage: `url(${ytplaceholder.src})`,
                                 backgroundPosition: "50%",
                             }}
+                            variants={fadeInUp}
+                            initial="hidden"
+                            whileInView="show"
+                            viewport={{ once: false, amount: 0.3 }}
                         >
                             <iframe
                                 src="https://www.youtube.com/embed/H7ClHApHc3o?rel=0&controls=1&autoplay=0&mute=0&start=0"
                                 frameBorder={0}
                                 className="absolute left-0 top-0 w-full h-full pointer-events-auto"
                             ></iframe>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
